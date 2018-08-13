7d08a863e2ff948462033b821532c909f9d11876
Glenn Dwiyatcita <dwiyatci@mail.com>
2018-08-13 18:17:45 +0200

    🎨 style: screw _x and _y in the widget spec (turned out they're not necessarily needed)

--- plugins/the-sink/constants.js
+++ plugins/the-sink/constants.js
@@ -10,8 +10,6 @@
       {
         name: 'Recent Alarms', // each widget is uniquely identified by the `name` prop
         title: 'Recent alarms',
-        _x: 0,
-        _y: 0,
         _width: 4,
         _height: 4, // we play by 12-column grid
         config: {} // widget config goes here
@@ -19,8 +17,6 @@
       {
         name: 'Map',
         title: 'Device locations',
-        _x: 4,
-        _y: 0,
         _width: 8,
         _height: 4,
         config: {}
